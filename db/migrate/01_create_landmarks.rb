1d32d054a1e834a78ddc2ce1bdb006f5555525d9
Christopher Wood <cjwood032@gmail.com>
2018-07-26 23:39:40 +0000

    automatically backed up by learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -3,7 +3,10 @@ class CreateLandmarks < ActiveRecord::Migration
     create_table :landmarks do |t|
       t.string :name
       t.integer :year_completed
+<<<<<<< HEAD
       t.integer :figure_id
+=======
+>>>>>>> 11196f1d6346d28135619010d82206f33f27b68e
     end
   end
 end
